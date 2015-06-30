fe3f4f8b87036028fc13d1d526e08afac86f3694
OpenShift System User <554f50b8fcf933a4b80000ea@ex-std-node238.prod.rhcloud.com>
2015-06-30 11:14:04 -0400

    Some commit message

--- server.js
+++ server.js
@@ -403,8 +403,12 @@ generaR(arrayDescrizioni);
 
 	})	
 	
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 
-app.listen('8081')
-console.log('Magic happens on port 8081');
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
 exports = module.exports = app; 	
+
